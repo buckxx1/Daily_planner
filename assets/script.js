080f1554ae6d707dd76fd41c628c80973e01fc0a
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-03-27 13:58:56 -0400

    finally working

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,7 @@
-//method for grabbing current time 
-var currentDay = moment().format("dddd, MMMM");
+// current day 
+var currentDay = moment().format("dddd, MMMM Do");
 
-//create hour time slots
-var hours = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
-
-// variables for each hour block
+// moment.js timeblocks
 var hour9 = moment().hour(9);
 var hour10 = moment().hour(10);
 var hour11 = moment().hour(11);
@@ -14,42 +11,40 @@ var hour14 = moment().hour(14);
 var hour15 = moment().hour(15);
 var hour16 = moment().hour(16);
 var hour17 = moment().hour(17);
+var hour18 = moment().hour(18);
 
+// Create an array of hour blocks for code generation
+var hours = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
 
-//pull from local storage and store in vars
+// local storage data or empty conatainer
+var events9 = JSON.parse(localStorage.getItem('hour9')) || "";
+var events10 = JSON.parse(localStorage.getItem('hour10')) || "";
+var events11 = JSON.parse(localStorage.getItem('hour11')) || "";
+var events12 = JSON.parse(localStorage.getItem('hour12')) || "";
+var events13 = JSON.parse(localStorage.getItem('hour13')) || "";
+var events14 = JSON.parse(localStorage.getItem('hour14')) || "";
+var events15 = JSON.parse(localStorage.getItem('hour15')) || "";
+var events16 = JSON.parse(localStorage.getItem('hour16')) || "";
+var events17 = JSON.parse(localStorage.getItem('hour17')) || "";
 
-var plannerItem9 = JSON.parse(localStorage.getItem('hour9')) || "";
-var plannerItem10 = JSON.parse(localStorage.getItem('hour10')) || "";
-var plannerItem11 = JSON.parse(localStorage.getItem('hour11')) || "";
-var plannerItem12 = JSON.parse(localStorage.getItem('hour12')) || "";
-var plannerItem13 = JSON.parse(localStorage.getItem('hour13')) || "";
-var plannerItem14 = JSON.parse(localStorage.getItem('hour14')) || "";
-var plannerItem15 = JSON.parse(localStorage.getItem('hour15')) || "";
-var plannerItem16 = JSON.parse(localStorage.getItem('hour16')) || "";
-var plannerItem17 = JSON.parse(localStorage.getItem('hour17')) || "";
-var plannerItem18 = JSON.parse(localStorage.getItem('hour18')) || "";
-
-//current day in html 
-$('#currentDay').text(currentDay)
-
-// make hour blocks anad pull data from local storage
-
-$.each(hours, function(index, value) {
-    plannerItem = [plannerItem9, plannerItem10, plannerItem11, plannerItem12, plannerItem13, plannerItem14, plannerItem15, plannerItem16, plannerItem17]
-    //appending the items into html
+// create timeblocks
+$.each(hours, function (index, value) {
+    events = [events9, events10, events11, events12, events13, events14, events15, events16, events17]
     $(".container").append("<div class='row'><div class='col-2 hour text-right' id='hour" +
         (index + 9) + "'><span>" + value.format("h A") + "</span></div><div class='col-8 event-group' id='timeblock" +
         (index + 9) + "'><textarea class='events col-12' id='eventblock" + (index + 9) + "'>" + events[index] + "</textarea></div>" +
         "<div class='col-2 save-delete' id='save-delete" + (index + 9) + "'><i class='fas fa-save' title='Save Event'></i> <i class='fas fa-trash' title='Remove Event'></i></div></div></div>");
 });
 
-// Check what time it is to color blocks correctly
+// display day 
+$("#currentDay").text(currentDay);
 
-//check current time
+// check time and display color
 var checkTime = function () {
     currentTime = moment().format("hh:mm:ss");
+    
 
-    //do a check of hour 9 (maybe replace isBetween with isSame)
+    
     if (moment().isBetween(hour9, hour10)) {
         $("#timeblock9").addClass("present");
     }
@@ -57,10 +52,10 @@ var checkTime = function () {
         $("#timeblock9").addClass("past");
     }
     else {
-        $("timeblock9").addClass("future");
+        $("#timeblock9").addClass("future");
     }
 
-    // check for hour 10
+
     if (moment().isBetween(hour10, hour11)) {
         $("#timeblock10").addClass("present");
     }
@@ -68,10 +63,10 @@ var checkTime = function () {
         $("#timeblock10").addClass("past");
     }
     else {
-        $("timeblock10").addClass("future");
+        $("#timeblock10").addClass("future");
     }
 
-    //check for hour 11
+
     if (moment().isBetween(hour11, hour12)) {
         $("#timeblock11").addClass("present");
     }
@@ -79,10 +74,9 @@ var checkTime = function () {
         $("#timeblock11").addClass("past");
     }
     else {
-        $("timeblock11").addClass("future");
+        $("#timeblock11").addClass("future");
     }
 
-    // check for hour 12 
     if (moment().isBetween(hour12, hour13)) {
         $("#timeblock12").addClass("present");
     }
@@ -90,10 +84,9 @@ var checkTime = function () {
         $("#timeblock12").addClass("past");
     }
     else {
-        $("timeblock12").addClass("future");
+        $("#timeblock12").addClass("future");
     }
 
-    //check for hour 13
     if (moment().isBetween(hour13, hour14)) {
         $("#timeblock13").addClass("present");
     }
@@ -101,10 +94,10 @@ var checkTime = function () {
         $("#timeblock13").addClass("past");
     }
     else {
-        $("timeblock13").addClass("future");
+        $("#timeblock13").addClass("future");
     }
 
-    //check for hour 14
+
     if (moment().isBetween(hour14, hour15)) {
         $("#timeblock14").addClass("present");
     }
@@ -112,10 +105,9 @@ var checkTime = function () {
         $("#timeblock14").addClass("past");
     }
     else {
-        $("timeblock14").addClass("future");
+        $("#timeblock14").addClass("future");
     }
 
-    //check for hour 15
     if (moment().isBetween(hour15, hour16)) {
         $("#timeblock15").addClass("present");
     }
@@ -123,10 +115,9 @@ var checkTime = function () {
         $("#timeblock15").addClass("past");
     }
     else {
-        $("timeblock15").addClass("future");
+        $("#timeblock15").addClass("future");
     }
 
-    //check for hour 16
     if (moment().isBetween(hour16, hour17)) {
         $("#timeblock16").addClass("present");
     }
@@ -134,10 +125,10 @@ var checkTime = function () {
         $("#timeblock16").addClass("past");
     }
     else {
-        $("timeblock16").addClass("future");
+        $("#timeblock16").addClass("future");
     }
 
-    //check for hour 17 
+
     if (moment().isBetween(hour17, hour18)) {
         $("#timeblock17").addClass("present");
     }
@@ -145,84 +136,46 @@ var checkTime = function () {
         $("#timeblock17").addClass("past");
     }
     else {
-        $("timeblock17").addClass("future");
+        $("#timeblock17").addClass("future");
     };
-}; 
+}
 
 
-//save custom events
 
-$("#save-delete9").on("click", "i.fa-save", function () {
-    var event9 = $("#eventblock9").val().trim();
-    localStorage.setItem('hour9', JSON.stringify(event9));
-})
-$("#save-delete10").on("click", "i.fa-save", function () {
-    var event10 = $("#eventblock10").val().trim();
-    localStorage.setItem('hour10', JSON.stringify(event10));
-})
-$("#save-delete11").on("click", "i.fa-save", function () {
-    var event11 = $("#eventblock11").val().trim();
-    localStorage.setItem('hour11', JSON.stringify(event11));
-})
-$("#save-delete12").on("click", "i.fa-save", function () {
-    var event12 = $("#eventblock12").val().trim();
-    localStorage.setItem('hour12', JSON.stringify(event12));
-})
-$("#save-delete13").on("click", "i.fa-save", function () {
-    var event13 = $("#eventblock13").val().trim();
-    localStorage.setItem('hour13', JSON.stringify(event13));
-})
-$("#save-delete14").on("click", "i.fa-save", function () {
-    var event14 = $("#eventblock14").val().trim();
-    localStorage.setItem('hour14', JSON.stringify(event14));
-})
-$("#save-delete15").on("click", "i.fa-save", function () {
-    var event15 = $("#eventblock15").val().trim();
-    localStorage.setItem('hour15', JSON.stringify(event15));
-})
-$("#save-delete16").on("click", "i.fa-save", function () {
-    var event16 = $("#eventblock16").val().trim();
-    localStorage.setItem('hour16', JSON.stringify(event16));
-})
-$("#save-delete17").on("click", "i.fa-save", function () {
-    var event17 = $("#eventblock17").val().trim();
-    localStorage.setItem('hour17', JSON.stringify(event17));
-});
 
-// add delete function
-
+// Add delete event function for each time block
 $("#save-delete9").on("click", "i.fa-trash", function () {
     localStorage.removeItem("hour9");
     $("#eventblock9").val("");
-})
+});
 $("#save-delete10").on("click", "i.fa-trash", function () {
     localStorage.removeItem("hour10");
     $("#eventblock10").val("");
-})
+});
 $("#save-delete11").on("click", "i.fa-trash", function () {
     localStorage.removeItem("hour11");
     $("#eventblock11").val("");
-})
+});
 $("#save-delete12").on("click", "i.fa-trash", function () {
     localStorage.removeItem("hour12");
     $("#eventblock12").val("");
-})
+});
 $("#save-delete13").on("click", "i.fa-trash", function () {
     localStorage.removeItem("hour13");
     $("#eventblock13").val("");
-})
+});
 $("#save-delete14").on("click", "i.fa-trash", function () {
     localStorage.removeItem("hour14");
     $("#eventblock14").val("");
-})
+});
 $("#save-delete15").on("click", "i.fa-trash", function () {
     localStorage.removeItem("hour15");
     $("#eventblock15").val("");
-})
+});
 $("#save-delete16").on("click", "i.fa-trash", function () {
     localStorage.removeItem("hour16");
     $("#eventblock16").val("");
-})
+});
 $("#save-delete17").on("click", "i.fa-trash", function () {
     localStorage.removeItem("hour17");
     $("#eventblock17").val("");
@@ -230,13 +183,53 @@ $("#save-delete17").on("click", "i.fa-trash", function () {
 
 
 
+// Add save event function for each time block
+$("#save-delete9").on("click", "i.fa-save", function () {
+    var event9 = $("#eventblock9").val().trim();
+    localStorage.setItem('hour9', JSON.stringify(event9));
+});
+$("#save-delete10").on("click", "i.fa-save", function () {
+    var event10 = $("#eventblock10").val().trim();
+    localStorage.setItem('hour10', JSON.stringify(event10));
+});
+$("#save-delete11").on("click", "i.fa-save", function () {
+    var event11 = $("#eventblock11").val().trim();
+    localStorage.setItem('hour11', JSON.stringify(event11));
+});
+$("#save-delete12").on("click", "i.fa-save", function () {
+    var event12 = $("#eventblock12").val().trim();
+    localStorage.setItem('hour12', JSON.stringify(event12));
+});
+$("#save-delete13").on("click", "i.fa-save", function () {
+    var event13 = $("#eventblock13").val().trim();
+    localStorage.setItem('hour13', JSON.stringify(event13));
+});
+$("#save-delete14").on("click", "i.fa-save", function () {
+    var event14 = $("#eventblock14").val().trim();
+    localStorage.setItem('hour14', JSON.stringify(event14));
+});
+$("#save-delete15").on("click", "i.fa-save", function () {
+    var event15 = $("#eventblock15").val().trim();
+    localStorage.setItem('hour15', JSON.stringify(event15));
+});
+$("#save-delete16").on("click", "i.fa-save", function () {
+    var event16 = $("#eventblock16").val().trim();
+    localStorage.setItem('hour16', JSON.stringify(event16));
+});
+$("#save-delete17").on("click", "i.fa-save", function () {
+    var event17 = $("#eventblock17").val().trim();
+    localStorage.setItem('hour17', JSON.stringify(event17));
+});
 
-//check the time and log it for my own cheeck 
+
+
+// This function runs but the time is never refreshed?
 setInterval(function () {
     $(".event-group .events").each(function (index, el) {
         checkTime(el);
-        console.log(currentTime)
+        
     });
+
 }, (1000 * 60));
 
 checkTime();
